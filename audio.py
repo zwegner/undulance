0d47ca58f21ea99ebe495454cd3b66bbb8777885
Zach Wegner <zwegner@gmail.com>
2014-06-12 18:14:46 -0500

    Various cleanups/features/optimizations

--- audio.py
+++ audio.py
@@ -9,17 +9,23 @@ import sys
 import threading
 
 sample_rate = 44100
+
 p = pyaudio.PyAudio()
-#    print(p.get_device_count())
-#    for i in range(p.get_device_count()):
-#        print(p.get_device_info_by_index(i))
+#print(p.get_device_count())
+#for i in range(p.get_device_count()):
+#    print(p.get_device_info_by_index(i))
 
 # open stream
 stream = p.open(format=pyaudio.paInt16,
-        output_device_index=2,
+        output_device_index=1,
         channels=1,
         rate=sample_rate,
         output=True)
+
+#p = subprocess.Popen(['play', '-r', '44100', '-b', '16', '-e', 'signed-integer', '-c', '1', '-t', 'raw', '-'],
+##p = subprocess.Popen(['play', '-t', 'raw', '-'],
+#        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+#stream = p.stdin
 
 def exit():
     sys.exit(0)
@@ -42,10 +48,12 @@ def operator(*params):
                 if not isinstance(self, Const):
                     a = fixup(a)
                 setattr(self, p, a)
+                if hasattr(a, 'eval'):
+                    setattr(self, '%s_eval' % p, a.eval)
             if hasattr(self, 'setup'):
                 self.setup()
         cls.__init__ = __init__
-        if hasattr(cls, '__str__'):
+        if not hasattr(cls, '__str__'):
             def __str__(self):
                 return '%s(%s)' % (self.__class__.__name__,
                         ', '.join(str(getattr(self, p)) for p in params))
@@ -67,16 +75,9 @@ class Const(Node):
 class Binop(Node):
     def __str__(self):
         return '(%s %s %s)' % (self.lhs, self.__class__.op, self.rhs)
-    def walk_tree(self):
-        yield self
-        for c in [self.lhs, self.rhs]:
-            for n in c.walk_tree():
-                yield n
 
-all_ops = []
 def binop(op):
     def deco(x):
-        all_ops.append(x)
         x.op = op
         return x
     return deco
@@ -84,22 +85,22 @@ def binop(op):
 @binop('+')
 class Add(Binop):
     def eval(self, ctx):
-        return self.lhs.eval(ctx) + self.rhs.eval(ctx)
+        return self.lhs_eval(ctx) + self.rhs_eval(ctx)
 
 @binop('-')
 class Sub(Binop):
     def eval(self, ctx):
-        return self.lhs.eval(ctx) - self.rhs.eval(ctx)
+        return self.lhs_eval(ctx) - self.rhs_eval(ctx)
 
 @binop('*')
 class Mul(Binop):
     def eval(self, ctx):
-        return self.lhs.eval(ctx) * self.rhs.eval(ctx)
+        return self.lhs_eval(ctx) * self.rhs_eval(ctx)
 
 @binop('/')
 class Div(Binop):
     def eval(self, ctx):
-        return self.lhs.eval(ctx) / self.rhs.eval(ctx)
+        return self.lhs_eval(ctx) / self.rhs_eval(ctx)
 
 # Add in operator overloading to Node class. Must be done after the child classes
 # are instantiated... weird.
@@ -124,11 +125,11 @@ class Osc(Node):
         self.phase = 0
         self.last_freq = None
     def eval(self, ctx):
-        freq = self.freq.eval(ctx)
+        freq = self.freq_eval(ctx)
         if freq != self.last_freq:
-            self.phase_inc = freq / ctx.sample_rate
+            self.ratio = freq / ctx.sample_rate
             self.last_freq = freq
-        self.phase += self.phase_inc
+        self.phase = ctx.sample * self.ratio
         self.phase -= int(self.phase)
         return self.eval_wave(self.phase)
     def __str__(self):
@@ -145,7 +146,7 @@ class Square(Osc):
 @operator('freq', 'pulse_width')
 class Pulse(Osc):
     def eval_wave(self, phase):
-        return 1 if phase > self.pulse_width.eval(ctx) else -1
+        return 1 if phase > self.pulse_width_eval(ctx) else -1
 
 class SawUp(Osc):
     def eval_wave(self, phase):
@@ -165,23 +166,40 @@ class Noise(Node):
         return 2 * random.random() - 1
 
 @operator('input', 'gate')
-class Envelope(Node):
+class ExpEnvelope(Node):
     def setup(self):
         self.last_gate = 0
         self.current = 0
     def eval(self, ctx):
-        gate = self.gate.eval(ctx) > 0
+        gate = self.gate_eval(ctx) > 0
         trigger = gate and not self.last_gate
         self.last_gate = gate
-        #if not gate:
-        #    self.current = 0
         if trigger:
             self.current = 1
         self.current *= .9999
-        return self.current * self.input.eval(ctx)
+        return self.current * self.input_eval(ctx)
 
-def EnvelopeBeat(input, beat):
-    return Envelope(input, Trigger(beat))
+@operator('input', 'time', 'gate')
+class Envelope(Node):
+    def setup(self):
+        self.last_gate = 0
+        self.current = 0
+        self.ratio = 0
+    def eval(self, ctx):
+        gate = self.gate_eval(ctx) > 0
+        trigger = gate and not self.last_gate
+        self.last_gate = gate
+        if trigger:
+            self.current = 1
+            self.ratio = 1 / (self.time_eval(ctx) * ctx.sample_rate)
+        self.current = max(0, self.current - self.ratio)
+        return self.current * self.input_eval(ctx)
+
+def EnvelopeBeat(input, time, beat):
+    return Envelope(input, time, Trigger(beat))
+
+def ExpEnvelopeBeat(input, beat):
+    return ExpEnvelope(input, Trigger(beat))
 
 @operator('note')
 class Diatonic(Node):
@@ -189,25 +207,55 @@ class Diatonic(Node):
     def setup(self):
         self.last_note = None
     def eval(self, ctx):
-        note = self.note.eval(ctx)
+        note = self.note_eval(ctx)
         if note != self.last_note:
             self.value = 256 * Diatonic.half_step ** (note - 40)
             self.last_note = note
         return self.value
 
+@operator('note', 'scale')
+class Scale(Node):
+    def eval(self, ctx):
+        note = int(self.note_eval(ctx))
+        while not self.scale_eval(ctx)[note % 12]:
+            note -= 1
+        return note
+
+major_notes = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
+#major_notes = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+scales = {root: (major_notes * 2)[root:root+12] for root in range(12)}
+@operator('root')
+class MajorScale(Node):
+    def eval(self, ctx):
+        return scales[self.root_eval(ctx)]
+
 @operator('bpm')
 class Beat(Node):
     def setup(self):
-        self.ratio = self.bpm.eval(ctx) / (ctx.sample_rate * 60)
+        self.ratio = self.bpm_eval(ctx) / (ctx.sample_rate * 60)
     def eval(self, ctx):
         return ctx.sample * self.ratio
+
+@operator('beat')
+class Rhythm(Node):
+    def eval(self, ctx):
+        return self.beat_eval(ctx)
+
+@operator('trigger', 'signal')
+class Sample(Node):
+    def setup(self):
+        self.sampled = 0
+    def eval(self, ctx):
+        if self.trigger_eval(ctx):
+            self.sampled = self.signal_eval(ctx)
+        return self.sampled
 
 @operator('beat')
 class Trigger(Node):
     def setup(self):
         self.last_beat = -1
     def eval(self, ctx):
-        beat = int(self.beat.eval(ctx))
+        beat = int(self.beat_eval(ctx))
         trigger = beat != self.last_beat
         self.last_beat = beat
         return trigger
@@ -215,7 +263,7 @@ class Trigger(Node):
 @operator('beat', 'args')
 class Switcher(Node):
     def eval(self, ctx):
-        return self.args[int(self.beat.eval(ctx)) % len(self.args)].eval(ctx)
+        return self.args[int(self.beat_eval(ctx)) % len(self.args)].eval(ctx)
 
 #class Historic:
 
@@ -223,17 +271,21 @@ ctx = Context()
 ctx.sample_rate = sample_rate
 
 beat = Beat(120)
-section = Switcher(beat / 4, [2, 4, 7/2, 8])
-eq = EnvelopeBeat(SawUp(Diatonic(section + Switcher(beat * section, [30, 32, 39, 42]))), beat * section)
-#eq += EnvelopeBeat(SawUp(110), beat / 2)
-#eq += EnvelopeBeat(Noise(), beat / 4)
-eq /= 20
+section = Switcher(beat / 4, [1, 3, 6, 8])
+bs = beat * section
+eq = Const(0)
+#eq += EnvelopeBeat(SawUp(Diatonic(section + Switcher(beat * section, [30, 32, 39, 42]))), beat * section)
+eq = EnvelopeBeat(SawUp(Diatonic(Scale(Sample(Trigger(bs), SawDown(Const(200) / (section + .5)) * 13 + 40), MajorScale(0)))), section / 10, bs)
+#eq += ExpEnvelopeBeat(Square(96), beat)
+#eq += ExpEnvelopeBeat(SawUp(256), beat * section)
+#eq += ExpEnvelopeBeat(Square(Diatonic(Sample(Trigger(beat*section), SawUp(beat*2 + 490)) * 4 + 30)), beat)
+#eq += ExpEnvelopeBeat(Noise(), beat / 4)
 
 ctx.sample = 0
 while True:
     try:
         ctx.sample += 1
         sample = eq.eval(ctx)
-        stream.write(struct.pack('h', int(sample * 65535)))
+        stream.write(struct.pack('h', int(sample * (65535 / 20.))))
     except KeyboardInterrupt:
         exit()
