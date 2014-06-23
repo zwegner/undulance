e7090e4d6b34c0f49e8a61d304a7d8087bb9320b
Zach Wegner <zwegner@gmail.com>
2014-06-23 15:25:44 -0500

    Trying to get audio latency down a bit

--- shell.py
+++ shell.py
@@ -24,7 +24,7 @@ if 0:
             output=True)
 else:
     p = subprocess.Popen(['sox', '-q', '-r', '44100', '-b', '16', '-e',
-            'signed-integer', '-c', '1', '-t', 'raw', '-', '-d'],
+            'signed-integer', '-c', '1', '-t', 'raw', '--buffer', '128', '-', '-d'],
             stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     stream = p.stdin
 
@@ -33,7 +33,7 @@ class InputThread(threading.Thread):
         global last_audio
         while True:
             try:
-                eq = input('> ')
+                input('> ')
                 last_audio = audio
                 imp.reload(audio)
                 audio.sample_rate = ctx.sample_rate
@@ -54,6 +54,7 @@ while True:
         ctx.sample += 1
         sample = audio.eq.eval(ctx)
         stream.write(struct.pack('h', int(sample * (65535 / 20.))))
+        stream.flush()
     except KeyboardInterrupt:
         sys.exit()
     except:
